f77a0257c13914afaca64f0b3c1ccdd9ec974776
joel-teo <joel.teo.2021@scis.smu.edu.sg>
2023-09-12 12:53:29 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/SPMG4T2/",
+  plugins: [vue()],
+})
