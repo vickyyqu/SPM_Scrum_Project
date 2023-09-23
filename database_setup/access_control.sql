23e9586c610f35d102d60fe46e4b36a50c55f886
Mohammad Fadhli <marvelouslyfadhli@gmail.com>
2023-09-24 02:01:44 +0800

    consolidated sql files into 1 file.

--- database_setup/access_control.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `Access_Control`;
-CREATE TABLE IF NOT EXISTS `Access_Control` (
-  `Access_ID` int NOT NULL,
-  `Access_Control_Name` varchar(20),
-  PRIMARY KEY (`Access_ID`)
-)
