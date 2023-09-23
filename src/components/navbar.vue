e4b9d78faab18eaf9f0eeed369f310252797cfbd
JustinPansacola <pansacolajustin@gmail.com>
2023-09-23 18:21:43 +0800

    Frontend Changes for Scrum-27

---
+++ src/components/navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar fixed-top " style="background-color: #22277A;" >
+        <div class="container-fluid" style="background-color: #22277A;">
+        <a class="navbar-brand" href="#" style="color: white;">All - In - One</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Role Listings</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">My Skills Profile</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">My Profile</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </div>
+    </nav>
+</template>
