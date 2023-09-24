a9e2d86757f1f81e8560eaa1b7f82145a029d598
Goh Zhi Yi <gohzhiyi77@gmail.com>
2023-09-25 00:45:09 +0800

    created template for add role listings

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # SQLAlchemy configuration
-# change accordingly whether you have "root" or "" as DB password
-# method naming: get, put, post, delete
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/sbrp_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/sbrp_test'
 db = SQLAlchemy(app)
 
 ##### DB Tables ######
@@ -19,37 +17,16 @@ class RoleListingTable(db.Model):
     Listing_ID = db.Column(db.Integer, primary_key=True)
     Role_Name = db.Column(db.String(255))
     Country = db.Column(db.String(255))
-    Department = db.Column(db.String(255))
+    Department =db.Column(db.String(255))
     Open_Window = db.Column(db.DateTime)
     Close_Window = db.Column(db.DateTime)
     # Add more columns as needed
 
-
-@app.route('/getAllRoleListings', methods=['GET'])
-def get_all_role_listings():
-    try:
-        # Fetch data from the database using SQLAlchemy
-        data = RoleListingTable.query.all()
-        data_dict = [{'id': item.Listing_ID, 'name': item.Role_Name, 'country': item.Country,
-            'dept': item.Department, 'OpenW': item.Open_Window, 'CloseW': item.Close_Window} for item in data]
-        return jsonify(data_dict)
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-
-@app.route('/postRoleListings', methods=['POST'])
-def post_role_listings():
-    data = request.get_json()
-    title = data.get('title')
-    content = data.get('content')
-
-    if title and content:
-        new_post = Post(title=title, content=content)
-        db.session.add(new_post)
-        db.session.commit()
-        return jsonify({'message': 'Post created successfully'})
-    else:
-        return jsonify({'error': 'Invalid data'}), 400
+class RoleSkillTable(db.Model):
+    __tablename__ = "Role_Skill"
+    Role_Name = db.Column(db.String(255), primary_key=True)
+    Skill_Name = db.Column(db.String(255), primary_key=True)
+    Proficiency_Level = db.Column(db.Integer)
 
 class StaffSkillTable(db.Model):
     __tablename__ = "Staff_Skill"
@@ -61,6 +38,8 @@ class StaffSkillTable(db.Model):
 
 ##### API Endpoints ######
 
+@app.route('/all', methods=['GET'])
+def get_data():
     try:
         # Fetch data from the database using SQLAlchemy
         data = RoleListingTable.query.all()
@@ -81,7 +60,7 @@ def get_allskills():
         return jsonify({'error': str(e)})
     
 @app.route('/getstaffskills/<string:staff_id>', methods=['GET'])
-def get_staffkills(staff_id):
+def get_staffskills(staff_id):
     try:
         # Fetch data from the database using SQLAlchemy
         data = StaffSkillTable.query.filter_by(Staff_ID=staff_id)
@@ -98,30 +77,28 @@ def get_staffkills(staff_id):
     
     except Exception as e:
         return jsonify({'error': str(e)})
-    
-    
 
-
-class SkillTable(db.Model):
-    __tablename__ = 'Skill'  # Replace with your table name
-    # Define your table columns here
-    Skill_Name = db.Column(db.String(255), primary_key=True)
-    Skill_Desc = db.Column(db.String(255))
-    # Add more columns as needed
-
-
-@app.route('/getAllSkills', methods=['GET'])
-def get_all_skills():
+@app.route('/getroleskills/<string:listing_id>', methods=['GET'])
+def get_roleskills(listing_id):
     try:
         # Fetch data from the database using SQLAlchemy
-        data = SkillTable.query.all()
-        data_dict = [{ 'skillName': item.Skill_Name, 'skillDesc': item.Skill_Desc } for item in data]
+        listing = RoleListingTable.query.filter_by(Listing_ID=listing_id).first()
+        if not listing:
+            return jsonify(
+                {
+                    "code": 404,
+                    "error": "Listing does not exist."
+                })
+        
+        role = listing.Role_Name
+        data = RoleSkillTable.query.filter_by(Role_Name=role)
+        data_dict = [{'skill' : item.Skill_Name, 'proficiency' : item.Proficiency_Level } for item in data]
+        
         return jsonify(data_dict)
+    
     except Exception as e:
         return jsonify({'error': str(e)})
-
-
-
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
