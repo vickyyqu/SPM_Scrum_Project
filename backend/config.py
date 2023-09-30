a16feb80a226e4a197ab23c167590a09e5d7fb38
vickyyqu <112297073+vickyyqu@users.noreply.github.com>
2023-09-30 14:34:26 +0800

    configure .env file, reformat backend

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import os
+
+# Load db variable from .env file into environment
+load_dotenv()
+database_url = os.getenv("DATABASE_URL")
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# SQLAlchemy configuration
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost/sbrp_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+db = SQLAlchemy(app)
