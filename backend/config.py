259f73b9c6dc82f3c1e6ecabc4267ba9a4722600
Goh Zhi Yi <gohzhiyi77@gmail.com>
2023-10-01 23:10:33 +0800

    completed post role listing service

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
