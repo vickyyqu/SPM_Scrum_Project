e25bf9d71195c5ac4494e8dade34dd5dcb981877
JustinPansacola <pansacolajustin@gmail.com>
2023-10-11 00:47:44 +0800

    Update ViewRoleListingDetails.vue

--- src/views/ViewRoleListingDetails.vue
+++ src/views/ViewRoleListingDetails.vue
@@ -184,5 +184,13 @@ export default {
     </div>
 </template>
 
-<style></style>
+<style>
+    body {
+    margin: 0;
+    display: flex;
+    place-items: center;
+    min-width: 320px;
+    min-height: 100vh;
+    }
+</style>
 
