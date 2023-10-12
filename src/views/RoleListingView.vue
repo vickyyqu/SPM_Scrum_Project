222a8a0bdde2ba4e9f809fce52b2b0948326c23f
JustinPansacola <pansacolajustin@gmail.com>
2023-10-12 15:42:44 +0800

    Update RoleListingView.vue

--- src/views/RoleListingView.vue
+++ src/views/RoleListingView.vue
@@ -67,5 +67,15 @@ export default {
     </div>
 </template>
 
-<style></style>
+<style>
+
+body {
+  margin: 0;
+  display: flex;
+  place-items: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
+
+</style>
 
