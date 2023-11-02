fc6387ece8718f26f58731a139cf215899d0e1cc
JustinPansacola <pansacolajustin@gmail.com>
2023-11-02 16:54:14 +0800

    Added Integration Tests for Sprint4

--- backend/functions.py
+++ backend/functions.py
@@ -2,27 +2,66 @@ def role_skill_match(staff_skills, role_skills):
     if staff_skills == None or role_skills == None:
         return None, None
     
+    roleSkillName = ""
+    staffSkillName =""
+    roleProfLevel = ""
+    staffProfLevel =""
+
+   
+    if len(role_skills) > 0 and len(staff_skills) > 0:
+        if "Skill_Name" in role_skills[0]:
+            roleSkillName = "Skill_Name"
+        else:
+            roleSkillName = "skill"
+
+        if "Skill_Name" in staff_skills[0]:
+            staffSkillName = "Skill_Name"
+        else:
+            staffSkillName = "skill"
+
+        if "Proficiency_Level" in role_skills[0]:
+            roleProfLevel = "Proficiency_Level"
+        else:
+            roleProfLevel = "proficiency"
+
+        if "Proficiency_Level" in staff_skills[0]:
+            staffProfLevel = "Proficiency_Level"
+        else:
+            staffProfLevel = "proficiency"
+
+    elif len(role_skills) == 0:
+        staffSkillName = "skill"
+        staffProfLevel = "proficiency"
+        roleSkillName = "Skill_Name"
+        roleProfLevel = "Proficiency_Level"  
+
+    elif len(staff_skills) == 0:
+        staffSkillName = "skill"
+        staffProfLevel = "proficiency"
+        roleSkillName = "Skill_Name"
+        roleProfLevel = "Proficiency_Level"  
+    
     skill_match_list = []
     overall_match = 0
     all_skills = []
     count = 0
 
     for role_skill in role_skills:
-        r_skill = role_skill["skill"].upper()
+        r_skill = role_skill[roleSkillName].upper()
 
         for staff_skill in staff_skills:
-            s_skill = staff_skill["skill"].upper()
+            s_skill = staff_skill[staffSkillName].upper()
 
             if r_skill == s_skill and r_skill not in all_skills:
                 all_skills.append(r_skill)
                 match = 100
 
-                if role_skill["proficiency"] > 0:
-                    match = min(staff_skill["proficiency"] / role_skill["proficiency"], 1) * 100
+                if role_skill[roleProfLevel] > 0:
+                    match = min(staff_skill[staffProfLevel] / role_skill[roleProfLevel], 1) * 100
 
                 out = {
-                    "skill": role_skill["skill"],
-                    "proficiency": role_skill["proficiency"],
+                    "skill": role_skill[roleSkillName],
+                    "proficiency": role_skill[roleProfLevel],
                     "match": round(match)
                 }
                 skill_match_list.append(out)
@@ -32,8 +71,8 @@ def role_skill_match(staff_skills, role_skills):
         if r_skill not in all_skills:
             all_skills.append(r_skill)
             out = {
-                "skill": role_skill["skill"],
-                "proficiency": role_skill["proficiency"],
+                "skill": role_skill[roleSkillName],
+                "proficiency": role_skill[roleProfLevel],
                 "match": 0
             }
             skill_match_list.append(out)
